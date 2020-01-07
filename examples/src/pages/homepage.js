2bf5ebaa13c33ae6eaada0073c3c565e2c9f2cd9
Alberto Parziale <alberto.parziale@caffeina.com>
2020-01-07 12:25:11 +0100

    update homepage example

---
+++ examples/src/pages/homepage.js
@@ -0,0 +1,102 @@
+import { qs } from '@okiba/dom'
+import Component from '@okiba/component'
+import SizesCache from '@okiba/sizes-cache'
+import EventManager from '@okiba/event-manager'
+import SmoothScroll from '@okiba/smooth-scroll'
+import ScrollManager from '@okiba/scroll-manager'
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  TextureLoader,
+  Geometry,
+  Vector3,
+  PointsMaterial,
+  AdditiveBlending,
+  Points
+} from 'three'
+
+class Scene3D extends Component {
+  constructor({ el }) {
+    super({ el })
+    this.init()
+    this.progress = 0
+    ScrollManager.on('scroll', this.onScroll)
+    EventManager.on('raf', this.onRaf)
+    EventManager.on('resize', this.onResize)
+  }
+
+  init() {
+    this.scene = new Scene()
+    this.camera = new PerspectiveCamera(75, SizesCache.window.innerWidth / SizesCache.window.innerHeight, 0.1, 2000)
+
+    this.renderer = new WebGLRenderer()
+    this.renderer.setSize(SizesCache.window.innerWidth, SizesCache.window.innerHeight)
+
+    document.body.appendChild(this.renderer.domElement)
+    Object.assign(this.renderer.domElement.style, {
+      position: 'fixed',
+      top: '0',
+      left: '0',
+      width: '100%',
+      zIndex: '-1'
+    })
+
+    const textureLoader = new TextureLoader()
+    const texture = textureLoader.load('./assets/cloud-particle.png')
+    const starsGeometry = new Geometry()
+
+    for (let i = 0; i < 2000; i ++) {
+      const star = new Vector3()
+      star.x = 1000 - Math.random() * 2000
+      star.y = 1000 - Math.random() * 2000
+      star.z = 1000 - Math.random() * 2000
+
+      starsGeometry.vertices.push(star)
+    }
+
+    const starsMaterial = new PointsMaterial({
+      size: 15.0,
+      map: texture,
+      color: 0x888888,
+      blending: AdditiveBlending,
+      depthTest: false,
+      transparent: true
+    })
+
+    this.starField = new Points(starsGeometry, starsMaterial)
+
+    this.scene.add(this.starField)
+  }
+
+  onRaf = () =>{
+    this.camera.position.x = this.progress * 500
+    this.camera.position.y = this.progress * 500
+    this.starField.rotation.y += 0.001
+    this.renderer.render(this.scene, this.camera)
+  }
+
+  onScroll = ({progress}) => {
+    this.progress = progress
+  }
+
+  onResize = () => {
+    this.renderer.setSize(SizesCache.window.width, SizesCache.window.height, false)
+    this.camera.aspect = SizesCache.window.width /  SizesCache.window.height
+    this.camera.updateProjectionMatrix()
+  }
+}
+
+const app = new Component({
+  el: qs('#app'),
+  components: [
+    {
+      ghost: true,
+      type: SmoothScroll
+    },
+    {
+      ghost: true,
+      type: Scene3D
+    }
+  ]
+})
